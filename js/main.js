896868fac6def0e6221e77b7b6f5ba30e5b81f21
rodrigo gonzalez <rodrigogonzalez2132@gmail.com>
2023-03-23 00:05:29 -0300

    se agrega proyecto3

--- js/main.js
+++ js/main.js
@@ -1,42 +1,48 @@
-const mates = [
-    {tipo:"mate imperial",precio:9500 ,stock:1000},
-   {tipo:"mate torpedo",precio:8000 ,stock:3000 },
-   {tipo:"mate camionero",precio:6000,stock :1500},
-
+const listaproductos =[
+{id:1, nombre:"mate imperial", precio:9500},
+{id:2, nombre:"mate camionero", precio:7500},
 ]
+const listacarrito=[]
 
-for(let i= 0;i< mates.length;i++){
-console.log(mates[i].tipo)
-console.log(mates[i].precio)
-console.log(mates[i].stock)
-}
+const contenedor_productos =document.getElementById("contenedor_productos")
+const contenedor_carrito =document.getElementById("contenedor_carrito")
 
-do{
-let tipo =prompt("ingrese el tipo de mate")
+listaproductos.forEach(productos =>{
+contenedor_productos.innerHTML += `
 
-if(isNaN(tipo)){
-console.log("no definido")
+<div class="card" style="width: 18rem;">
+<img src="https://http2.mlstatic.com/D_NQ_NP_702437-MLA48733352973_012022-O.webp" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">${productos.nombre}</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href="#" class="btn btn-primary" id=matesimperial${productos.id}" >añadir al carrito</a>
+</div>
+</div>
 
-}
+`
+
+})
+
+listaproductos.forEach(productos => {
+const producto_enespera = document.getElementById(`matesimperial${producto.id}`)
+
+producto_enespera.addEventListener("click",()=>{
+    listacarrito.push(productos)
+
+    listacarrito.forEach(productos=>{
 
 
+        contenedor_carrito.innerHTML=""
 
+        contenedor_carrito.innerHTML+=`<div class="card" style="width: 18rem;">
+        <img src="https://http2.mlstatic.com/D_NQ_NP_702437-MLA48733352973_012022-O.webp" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${productos.nombre}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary" id=matesimperial${productos.id}" >añadir al carrito</a>
+        </div>
+        </div>`
+    })
+})
 
-
-
-if(mates.some(producto=>producto.tipo===tipo)){
-
-let cantidad=prompt("Que tipo de Mate desea?")
-// para buscar similitud en el tipo de mate
-const producto=mates.find(producto=>producto.tipo==producto)
-producto.cantidad=cantidad;
-
-CompraUsuario.push(producto)
-}
-else{
-console.log("El tipo de mate No existe")
-}
-
-rta =prompt("¿desea ver otro mate?si/no")
-
-}while(rta !="no")
+})
